a1d252415197cc0e9d3f759cc8ba9c992a103d96
Khachatur Gharibyan <khachatur.gharibyan@gmail.com>
2017-05-30 11:43:45 +0400

    RO-906 # add type field now you can set type featured, demos

--- models/projects.js
+++ models/projects.js
@@ -11,21 +11,21 @@ const Projects = new keystone.List('projects', {
 	map: { name: 'name'},
 });
 
-Projects.add({
-	state: { type: Types.Select, options: 'pending, approved, rejected', default: 'pending', index: true },
-	name: { type: String, index: true, required: true },
-	root: { type: String, readonly: true, noedit:true},
-	owner: { type: String, readonly: true, noedit:true},
-	displayName: { type: String, required: true, default:''},
-	domain: { type: String, },
-	description: { type: Types.Html, wysiwyg:false, height: 150, required: true, default:''},
-	updatedAt: { type: Types.Date, readonly: true, noedit:true},
-	createdAt: { type: Types.Date, readonly:true, noedit:true},
-	public: { type: Types.Boolean},
-	publishedPublic:{type: Types.Boolean, noedit:true},
-	publishDate: { type: Types.Date, readonly:true, noedit:true},
+Projects.add(
+	{state: { type: Types.Select, options: 'pending, approved, rejected', default: 'pending', index: true }},
+	{type: { type: Types.Select, options: 'demos, featured', default: '', index: true }},
+	{name: { type: String, index: true, required: true }},
+	{root: { type: String, readonly: true, noedit:true}},
+	{owner: { type: String, readonly: true, noedit:true}},
+	{displayName: { type: String, required: true, default:''}},
+	{domain: { type: String, readonly: true, noedit:true }},
+	{description: { type: Types.Html, wysiwyg:false, height: 150, required: true, default:''}},
+	{updatedAt: { type: Types.Date, readonly: true, noedit:true}},
+	{createdAt: { type: Types.Date, readonly:true, noedit:true}},
+	{public: { type: Types.Boolean}},
+	{publishedPublic:{type: Types.Boolean, noedit:true}},
+	{publishDate: { type: Types.Date, readonly:true, noedit:true}}
+);
 
-});
-
-Projects.defaultColumns = 'name, root, owner, updatedAt, state';
+Projects.defaultColumns = 'name, root, owner, domain, updatedAt, publishedPublic, state, type';
 Projects.register();
