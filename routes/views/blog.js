21084d830a53548f1da06116041a8453b2738f02
Raffi Hovhannisyan <reinchard1488@gmail.com>
2017-07-12 18:30:59 +0400

    RO-1066 # Blog implementation

--- routes/views/blog.js
+++ routes/views/blog.js
@@ -1,52 +1,53 @@
 var keystone = require('keystone');
 var async = require('async');
+const configFile = process.env.NODE_ENV || 'local';
 
+const ENV = require('dotenv').config({ path: `./env/.${configFile}` });
 exports = module.exports = function (req, res) {
 
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 
 	// Init locals
+	locals.moment = require('moment');
 	locals.section = 'blog';
 	locals.filters = {
-		category: req.params.category,
+		tag: req.params.tag,
 	};
 	locals.data = {
 		posts: [],
 		categories: [],
 	};
-
+	locals.apiRoute = ENV.parsed.API;
 	// Load all categories
-	view.on('init', function (next) {
-
-		keystone.list('PostCategory').model.find().sort('name').exec(function (err, results) {
-
-			if (err || !results.length) {
-				return next(err);
-			}
-
-			locals.data.categories = results;
-
-			// Load the counts for each category
-			async.each(locals.data.categories, function (category, next) {
-
-				keystone.list('Post').model.count().where('categories').in([category.id]).exec(function (err, count) {
-					category.postCount = count;
-					next(err);
-				});
-
-			}, function (err) {
-				next(err);
-			});
-		});
-	});
+	// view.on('init', function (next) {
+	// 	keystone.list('PostCategory').model.find().sort('name').exec(function (err, results) {
+    //
+	// 		if (err || !results.length) {
+	// 			return next(err);
+	// 		}
+    //
+	// 		locals.data.categories = results;
+    //
+	// 		// Load the counts for each category
+	// 		async.each(locals.data.categories, function (category, next) {
+    //
+	// 			keystone.list('Post').model.count().where('categories').in([category.id]).exec(function (err, count) {
+	// 				category.postCount = count;
+	// 				next(err);
+	// 			});
+    //
+	// 		}, function (err) {
+	// 			next(err);
+	// 		});
+	// 	});
+	// });
 
 	// Load the current category filter
 	view.on('init', function (next) {
-
-		if (req.params.category) {
-			keystone.list('PostCategory').model.findOne({ key: locals.filters.category }).exec(function (err, result) {
-				locals.data.category = result;
+		if (req.params.tag) {
+			keystone.list('Tags').model.findOne({slug: locals.filters.tag.toLowerCase()}).exec(function (err, result) {
+				locals.data.tag = result;
 				next(err);
 			});
 		} else {
@@ -66,14 +67,30 @@ exports = module.exports = function (req, res) {
 			},
 		})
 			.sort('-publishedDate')
-			.populate('author categories');
+			.populate('author categories tags');
 
-		if (locals.data.category) {
-			q.where('categories').in([locals.data.category]);
+		if (locals.data.tag) {
+			q.where('tags').in([locals.data.tag]);
 		}
 
 		q.exec(function (err, results) {
+			let random = [];
+			let featured = [];
+			results.results.map(post => {
+				if (post.categories.length > 0) {
+					return post.categories.map(ft => {
+						if (ft.name === 'featured')
+							featured.push(post)
+					});
+				} else {
+					random.push(post)
+				}
+			});
 			locals.data.posts = results;
+			locals.data.posts = {
+				featured,
+				random,
+			};
 			next(err);
 		});
 	});
