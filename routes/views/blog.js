368dedbd83389da3d222d4f187b5bf76c861811a
Raffi Hovhannisyan <reinchard1488@gmail.com>
2017-08-08 10:36:27 +0400

    RO-1226 # blog mobile

--- routes/views/blog.js
+++ routes/views/blog.js
@@ -3,75 +3,76 @@ var async = require('async');
 const configFile = process.env.NODE_ENV || 'local';
 
 const ENV = require('dotenv').config({ path: `./env/.${configFile}` });
-exports = module.exports = function (req, res) {
+exports = module.exports = function(req, res) {
 
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
+    var view = new keystone.View(req, res);
+    var locals = res.locals;
 
-	// Init locals
-	locals.moment = require('moment');
-	locals.section = 'blog';
-	locals.filters = {
-		tag: req.params.tag,
-	};
-	locals.data = {
-		posts: [],
-		categories: [],
-	};
-	locals.apiRoute = ENV.parsed.API;
-	locals.hst = ENV.parsed.hst;
-	//Load posts by tag name
-	view.on('init', function (next) {
-		if (req.params.tag) {
-			keystone.list('Tags').model.findOne({slug: locals.filters.tag.toLowerCase()}).exec(function (err, result) {
-				locals.data.tag = result;
-				next(err);
-			});
-		} else {
-			next();
-		}
-	});
+    // Init locals
+    locals.moment = require('moment');
+    locals.section = 'blog';
+    locals.filters = {
+        tag: req.params.tag,
+    };
+    locals.data = {
+        posts: [],
+        categories: [],
+    };
+    locals.apiRoute = ENV.parsed.API;
+    locals.hst = ENV.parsed.hst;
+    locals.blog_url = ENV.parsed.BLOG_URL;
+    //Load posts by tag name
+    view.on('init', function(next) {
+        if (req.params.tag) {
+            keystone.list('Tags').model.findOne({ slug: locals.filters.tag.toLowerCase() }).exec(function(err, result) {
+                locals.data.tag = result;
+                next(err);
+            });
+        } else {
+            next();
+        }
+    });
 
-	// Load the posts
-	view.on('init', function (next) {
+    // Load the posts
+    view.on('init', function(next) {
 
-		var q = keystone.list('Post').paginate({
-			page: req.query.page || 1,
-			perPage: 10,
-			maxPages: 10,
-			filters: {
-				state: 'published',
-			},
-		})
-			.sort('-publishedDate')
-			.populate('author categories tags');
+        var q = keystone.list('Post').paginate({
+                page: req.query.page || 1,
+                perPage: 10,
+                maxPages: 10,
+                filters: {
+                    state: 'published',
+                },
+            })
+            .sort('-publishedDate')
+            .populate('author categories tags');
 
-		if (locals.data.tag) {
-			q.where('tags').in([locals.data.tag]);
-		}
+        if (locals.data.tag) {
+            q.where('tags').in([locals.data.tag]);
+        }
 
-		q.exec(function (err, results) {
-			let random = [];
-			let featured = [];
-			results.results.map(post => {
-				if (post.categories.length > 0) {
-					return post.categories.map(ft => {
-						if (ft.name === 'featured')
-							featured.push(post)
-					});
-				} else {
-					random.push(post)
-				}
-			});
-			locals.data.posts = results;
-			locals.data.posts = {
-				featured,
-				random,
-			};
-			next(err);
-		});
-	});
+        q.exec(function(err, results) {
+            let random = [];
+            let featured = [];
+            results.results.map(post => {
+                if (post.categories.length > 0) {
+                    return post.categories.map(ft => {
+                        if (ft.name === 'featured')
+                            featured.push(post)
+                    });
+                } else {
+                    random.push(post)
+                }
+            });
+            locals.data.posts = results;
+            locals.data.posts = {
+                featured,
+                random,
+            };
+            next(err);
+        });
+    });
 
-	// Render the view
-	view.render('blog');
+    // Render the view
+    view.render('blog');
 };
