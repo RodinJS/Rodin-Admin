735eadb1adbd571e81c190e0322bc540a84de92a
Raffi Hovhannisyan <reinchard1488@gmail.com>
2017-08-16 15:39:43 +0400

    RO-1388 # Menu items for platform header menu

--- models/MenuItem.js
+++ models/MenuItem.js
@@ -5,15 +5,18 @@ const Types = keystone.Field.Types;
  * Product Model
  */
 const MenuItem = new keystone.List('cms_menuitem', {
-	autokey: {path: 'slug', from: 'name', unique: true},
-	defaultColumns: 'name, key, menus, href, active'
+    autokey: { path: 'slug', from: 'name', unique: true },
+    defaultColumns: 'name, key, menus, href, active'
 });
 
 MenuItem.add({
-	name: {type: String, required: true},
-	slug: {type: String, readonly: true},
-	href: {type: Types.Url, required: true, initial: true},
-	active: {type: Boolean, default: true}
+    name: { type: String, required: true },
+    slug: { type: String, readonly: true },
+    state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
+    href: { type: Types.Url, required: true, initial: true },
+    active: { type: Boolean, default: true },
+    position: { type: Number },
+    page: { type: Types.Relationship, ref: 'cms_pages' },
 });
-
+MenuItem.defaultColumns = 'name, slug|20%, state|20%, position|20%';
 MenuItem.register();
