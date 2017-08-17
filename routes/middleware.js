9fdb8de0682cbc610e3ceb62d862ce47f4895773
Raffi Hovhannisyan <reinchard1488@gmail.com>
2017-08-17 11:12:12 +0400

    RO-1345 # check cookies

--- routes/middleware.js
+++ routes/middleware.js
@@ -21,45 +21,46 @@ const ENV = require('dotenv').config({ path: `./env/.${configFile}` });
 	the navigation in the header, you may wish to change this array
 	or replace it with your own templates / logic.
 */
-exports.initLocals = function (req, res, next) {
-	res.locals.hidden = req.url.startsWith('/blog');
-	res.locals.navLinks = [
-		{ label: 'Home', key: 'home', href: '/' },
-		{ label: 'Blog', key: 'blog', href: '/blog' },
-		{ label: 'Gallery', key: 'gallery', href: '/gallery' },
-		{ label: 'Contact', key: 'contact', href: '/contact' },
-	];
-	res.locals.user = req.user;
-	res.locals.HST =  ENV.parsed.HST;
-	res.locals.blog_url = ENV.parsed.BLOG_URL;
-	res.locals.current = req.url
-	next();
+exports.initLocals = function(req, res, next) {
+    console.log(req.headers.cookie)
+    res.locals.hidden = req.url.startsWith('/blog');
+    res.locals.navLinks = [
+        { label: 'Home', key: 'home', href: '/' },
+        { label: 'Blog', key: 'blog', href: '/blog' },
+        { label: 'Gallery', key: 'gallery', href: '/gallery' },
+        { label: 'Contact', key: 'contact', href: '/contact' },
+    ];
+    res.locals.user = req.user;
+    res.locals.HST = ENV.parsed.HST;
+    res.locals.blog_url = ENV.parsed.BLOG_URL;
+    res.locals.current = req.url
+    next();
 };
 
 
 /**
 	Fetches and clears the flashMessages before a view is rendered
 */
-exports.flashMessages = function (req, res, next) {
-	var flashMessages = {
-		info: req.flash('info'),
-		success: req.flash('success'),
-		warning: req.flash('warning'),
-		error: req.flash('error'),
-	};
-	res.locals.messages = _.some(flashMessages, function (msgs) { return msgs.length; }) ? flashMessages : false;
-	next();
+exports.flashMessages = function(req, res, next) {
+    var flashMessages = {
+        info: req.flash('info'),
+        success: req.flash('success'),
+        warning: req.flash('warning'),
+        error: req.flash('error'),
+    };
+    res.locals.messages = _.some(flashMessages, function(msgs) { return msgs.length; }) ? flashMessages : false;
+    next();
 };
 
 
 /**
 	Prevents people from accessing protected pages when they're not signed in
  */
-exports.requireUser = function (req, res, next) {
-	if (!req.user) {
-		req.flash('error', 'Please sign in to access this page.');
-		res.redirect('/keystone/signin');
-	} else {
-		next();
-	}
+exports.requireUser = function(req, res, next) {
+    if (!req.user) {
+        req.flash('error', 'Please sign in to access this page.');
+        res.redirect('/keystone/signin');
+    } else {
+        next();
+    }
 };
